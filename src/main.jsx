fb6bddb99f711edcf61e14da750d364ef62e9fc7
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-13 23:11:39 +0600

    link added

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,11 @@ import router from './Routes/Routes.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}> 
+  <div className='max-w-7xl mx-auto'>
+      <React.StrictMode>
+      <RouterProvider router={router}> 
 
-    </RouterProvider>
-  </React.StrictMode>,
+      </RouterProvider>
+    </React.StrictMode>,
+  </div>
 )
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../../../assets/logo.svg'
 
 const Navbar = () => {
+    const navItems = <> 
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+      <li><Link to='/services'>Services</Link></li>
+      <li><Link to='/blog'>Blog</Link></li>
+      <li><Link to='/contact'>Contact</Link></li>
+     </>
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -25,68 +34,16 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navItems}
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <Link to='/' className="btn btn-ghost normal-case text-xl">
+            <img src={logo} alt="" />
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+        {navItems}
         </ul>
       </div>
       <div className="navbar-end">
