9f5d66aff7c5bcb21dfd1c5d03de884b3d6a314f
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-13 21:02:32 +0600

    footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Outlet> </Outlet>
+            <Footer> </Footer>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
       path:'/',
       element:<Main> </Main>,
+      children: [
+       {
+         path:'/',
+         element:<Home> </Home>
+       },
+      ]
       
     },
     {
