7aa2a77ef456ee367b61b3044cd59542b2db2f5f
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-14 19:55:46 +0600

    json data added

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <div>
+        <div className='mt-4'>
             <div className='text-center'>
               <h3 className='text-xl text-color-primary font-bold '>Services</h3>
               <h2 className='text-4xl font-bold '>Our Service Area</h2>
