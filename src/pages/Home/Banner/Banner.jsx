50bbe50fddac121356d620a2eeea4da7bdf055b5
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-14 14:38:18 +0600

    banner slider complete

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import image1 from '../../../assets/images/banner/1.jpg'
+import image1 from '../../../assets/images/banner/5.jpg'
 import image2 from '../../../assets/images/banner/2.jpg'
 import image3 from '../../../assets/images/banner/3.jpg'
 import image4 from '../../../assets/images/banner/4.jpg'
+import image5 from '../../../assets/images/banner/1.jpg'
+import image6 from '../../../assets/images/banner/6.jpg'
 const Banner = () => {
   return (
     <div className="carousel w-full h-[600px]">
@@ -26,7 +28,7 @@ const Banner = () => {
 
 
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-          <a href="#slide4" className="btn btn-circle mr-5">
+          <a href="#slide6" className="btn btn-circle mr-5">
             ❮
           </a>
           <a href="#slide2" className="btn btn-circle">
@@ -84,6 +86,7 @@ const Banner = () => {
           </a>
         </div>
       </div>
+
       <div id="slide4" className="carousel-item relative w-full">
         <img
           src={image4}
@@ -104,11 +107,68 @@ const Banner = () => {
           <a href="#slide3" className="btn btn-circle mr-5">
             ❮
           </a>
+          <a href="#slide5" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+
+
+      <div id="slide5" className="carousel-item relative w-full">
+        <img
+          src={image5}
+          className="w-full"
+        />
+         <div className="absolute h-full flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+         <div className="text-white space-y-7 pl-12 w-2/4">
+           <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+           <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+
+           <div>
+           <button className="btn btn-primary normal-case bg-[#FF3811] border-2 border-[#FF3811] hover:bg-transparent hover:border-[#FF3811] mr-5 rounded">Discover More</button>
+           <button className="btn btn-outline btn-transparent rounded border-2 border-stone-400 text-white normal-case">Latest Project</button>
+           </div>
+         </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide4" className="btn btn-circle mr-5">
+            ❮
+          </a>
+          <a href="#slide6" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+
+
+      <div id="slide6" className="carousel-item relative w-full">
+        <img
+          src={image6}
+          className="w-full"
+        />
+         <div className="absolute h-full flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+         <div className="text-white space-y-7 pl-12 w-2/4">
+           <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+           <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+
+           <div>
+           <button className="btn btn-primary normal-case bg-[#FF3811] border-2 border-[#FF3811] hover:bg-transparent hover:border-[#FF3811] mr-5 rounded">Discover More</button>
+           <button className="btn btn-outline btn-transparent rounded border-2 border-stone-400 text-white normal-case">Latest Project</button>
+           </div>
+         </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide5" className="btn btn-circle mr-5">
+            ❮
+          </a>
           <a href="#slide1" className="btn btn-circle">
             ❯
           </a>
         </div>
       </div>
+
+
+
     </div>
   );
 };
