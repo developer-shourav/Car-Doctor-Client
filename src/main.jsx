7b77800f04d4e772550bf1a3aa9bb2d426e5bd39
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-15 23:45:52 +0600

    auth providers setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Routes.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
+import AuthProviders from "./providers/AuthProviders";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='max-w-7xl mx-auto'>
-      <React.StrictMode>
-      <RouterProvider router={router}> 
-
-      </RouterProvider>
-    </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <AuthProviders>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProviders>
+    </React.StrictMode>
+    ,
   </div>
-)
+);
