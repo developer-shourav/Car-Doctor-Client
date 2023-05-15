6461ad92d6ee4d04184d83d5efc1f763bf2fb136
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-15 23:31:02 +0600

    auth context added

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -2,16 +2,26 @@ import React, { createContext } from 'react';
 import app from '../assets/firebase/firebase.config';
 import {getAuth} from 'firebase/auth';
 
-const AuthContext = createContext();
-const auth = getAuth(app)
 
-const AuthProviders = () => {
+export const AuthContext = createContext();
+
+const auth = getAuth(app)
+const AuthProviders = ({children}) => {
+    const user = {
+        displayName:'Shourav',
+         photo:'https://media.licdn.com/dms/image/C4E03AQGTYvY1e7x3UA/profile-displayphoto-shrink_800_800/0/1638112904151?e=2147483647&v=beta&t=v9Hf-uGwvnl-wcxp2eyRcLFb7en1d6S9I3AEcuCDTv8'
+        };
+
     const authInfo = {
-      user:{displayName:'Shourav', photo:'https://media.licdn.com/dms/image/C4E03AQGTYvY1e7x3UA/profile-displayphoto-shrink_800_800/0/1638112904151?e=2147483647&v=beta&t=v9Hf-uGwvnl-wcxp2eyRcLFb7en1d6S9I3AEcuCDTv8'}
+        user,
+        auth
+
     };
+
+
     return (
         <AuthContext.Provider value={authInfo}> 
-
+           {children}
         </AuthContext.Provider>
     );
 };
