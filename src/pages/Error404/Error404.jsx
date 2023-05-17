181a95cf3f2f002ae3f09685da2e3710a30286aa
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-17 22:43:13 +0600

    separate 404 page added

---
+++ src/pages/Error404/Error404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <div className='container mx-auto'>
+            <h4 className='error-page-style'>404</h4>
+        </div>
+    );
+};
+
+export default Error404;
