a3553c37da6aabb87c84998807a83b1d27677c6d
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-17 22:47:50 +0600

    page style added

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => {
     return (
         <div className='container mx-auto'>
             <h4 className='error-page-style'>404</h4>
+            <h2 className='text-3xl font-bold text-red-600 text-center'>Something Went wrong. Page not found !!</h2>
+            <br />
+            <Link className='btn btn-outline ' to='/home'> Back To Home</Link>
         </div>
     );
 };
